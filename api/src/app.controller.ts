92749eeda187066a5a342390e7b57ec51342a709
Bulat Kidasov <kidasov@gmail.com>
2020-04-19 16:30:41 +0400

    [Backend] Enabled cors

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth,guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    console.log('req', req)
     return req.user;
   }
 }
