86e73c631e69ec68e1cdd24a4c1e8f2a914b7b62
Bulat Kidasov <kidasov@gmail.com>
2020-05-03 20:37:10 +0400

    Added typer orm, setup db connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
@@ -6,8 +7,20 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'example',
+      database: 'postgres',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [AppController, ProductsController],
   providers: [AppService],
 })
+
 export class AppModule {}
