58ae86509ab776b07b9e73cb42ea43580a3d6132
kidasov <kidasov@gmail.com>
2020-12-31 19:30:33 +0400

    Make click outside directive global

--- web/src/main.js
+++ web/src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { clickOutside } from "./directives";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+
+Vue.directive("click-outside", clickOutside);
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
